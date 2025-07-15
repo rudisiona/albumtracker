ceea30e9fb037103664ea48aa3351fed582da998
rudisiona <rsmageo@gmail.com>
2025-07-14 18:00:40 -0700

    some style

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const router = express.Router();
+const router = express.Router({ mergeParams : true });
 
 const User = require('../models/user.js')
 
@@ -8,4 +8,16 @@ router.get('/party', async(req, res) => {
     res.render('users/index.ejs', { users})
 })
 
+router.get('/', async (req, res) => {
+   try { const user = await User.findById(req.params.userId)
+    if(!user) { 
+        return res.status(404).send("user not found");
+    } 
+        res.render('users/show.ejs', { user });
+} catch(err) {
+    console.error('error fetching user:', err)
+    res.status(500).send('internal server error')
+}
+
+})
 module.exports = router
